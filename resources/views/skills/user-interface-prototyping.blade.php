5a20b0f66f4640fe4653893839081a908087736b
allurisriakhil <allurisriakhil@gmail.com>
2020-07-27 08:33:22 -0400

    Update user-interface-prototyping.blade.php

--- resources/views/skills/user-interface-prototyping.blade.php
+++ resources/views/skills/user-interface-prototyping.blade.php
@@ -16,54 +16,59 @@
                         <article class="article">
                             <h2 class="h1 mt-4">USER INTERFACE PROTOTYPE</h2>
 
-                <p title="Name of Skill :">
+                <h3 class="h2">Name of Skill</h3>    
+                
+                <p>
 
                 USER INTERFACE PROTOTYPE
 
                 </p>
                
-                <p title="Classification of Skill :">
+               <h3 class="h2">Classification of Skill :</h3>  
+               
+               <p>
+               
                 This skill can be classified as knowledge based skill because the skill is based on the knowledge of specific subject, procedures and information necessary to perform particular tasks.
                 Which can also be acquired with the help of education, training or job experience.
                 </p>
 
-                <p title="Prerequisites for Skill :">
+              <h3>Prerequisites for User Inteface Prototyping</h3> 
+              <p>
 
-                <pre>
-                1. Understanding of Software Requirements.
+                1. Understanding of Software Requirements.<br>
 
-                2. Understanding of Use case Diagrams. 
+                2. Understanding of Use case Diagrams. <br>
 
-                3. Creative Thinking and Attention to the details by understanding the underlying bussiness.
+                3. Creative Thinking and Attention to the details by understanding the underlying bussiness.<br>
 
-                4. Well understanding on the UI design principles
+                4. Well understanding on the UI design principles <br>
 
-                5. Knowledge on use of prototyping tools.
-                </pre>
+                5. Knowledge on use of prototyping tools. <br>
+                
+                
                 </p>
 
-                <p title="Related Software Engineering Areas  :">
-                <pre>
+                 <h3>Related Software Engineering Areas</h3> 
+               
+                <p>
+                
+                1.  Software Testing - Usability Testing <br>
 
-                1.  Software Testing - Usability Testing
+                2.  Software Design  - User Inteface Designing <br>
 
-                2.  Software Design  - User Inteface Designing
+                3.  Software Engineering Process - As part of Software Design Lifecycle <br>
 
-                3.  Software Engineering Process - As part of Software Design Lifecycle
-
-
-                </pre>
                 </p>
 
-                <p title="Rationale for Skill  :">
-
-                <pre>
-
-                For any software development there is need of prototypes in order to know the design, look and end product of the software system in case of software development, elicitation of requirements, use case diagrams and UML model diagrams helps to have an understanding of the system for all the stakeholders involved in the software design lifecycle.
+              <h3>Rationale for Skill</h3>   
+               
+                 <p>
+                 
+                For any software development there is need of prototypes in order to know the design, look and end product of the software system in case of software development, elicitation of requirements, use case diagrams and UML model diagrams helps to have an understanding of the system for all the stakeholders involved in the software design lifecycle.<br>
 
                 But in case of web development along with the backend functionalities another important criteria is user interface using which a client or an customer interacts with the system based on how simple and user friendly is the interface it could attract more no of customers. So in order to know the design of user interface prior to the development of the final model a prototype is made to check the look and feel of the website which is helpful for the stakeholders to approve the design before the implementation which decreases both the cost and effort that are invested into development of final model.
 
-                </pre>
+               
 
                 </p>
                         </article>
